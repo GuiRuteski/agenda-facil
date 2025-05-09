c58ab24dc1c1bac2a80af51be3a6e47f8d369d5f
Guilherme Ruteski <guilhermeruteski@gmail.com>
2025-05-09 18:35:05 -0300

    feat: implementa tela de login e configura rotas básicas

--- backend/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .paciente import Paciente  # Apenas importa a classe Paciente
-from . import db  # Certifique-se de que o db foi importado aqui sem causar cíclicos
-
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-from app.models.paciente import Paciente
